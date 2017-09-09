a8bcaeb6a9fdbb6db7d1b1c3e4264acbbde6f1f8
Daniel Malone <danieljmalone@gmail.com>
2017-09-09 16:15:24 -0500

    make email validation work

--- app/src/main/java/com/finepointmobile/codeforjim/MainActivity.java
+++ app/src/main/java/com/finepointmobile/codeforjim/MainActivity.java
@@ -1,5 +1,8 @@
 package com.finepointmobile.codeforjim;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,6 +11,10 @@ import android.widget.EditText;
 
 import com.jakewharton.rxbinding2.InitialValueObservable;
 import com.jakewharton.rxbinding2.widget.RxTextView;
+import com.jakewharton.rxbinding2.widget.TextViewTextChangeEvent;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
@@ -18,11 +25,14 @@ import io.reactivex.SingleObserver;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 
+import static com.jakewharton.rxbinding2.widget.RxTextView.textChanges;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
     EditText mUsername;
+    EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mUsername = (EditText) findViewById(R.id.username);
+        email = (EditText) findViewById(R.id.email_validation);
 
         Single first = Single.just("Hello");
 
@@ -82,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
                 for (int i = 500; i <= 1000; i++) {
                     e.onNext(i);
                 }
+                e.onComplete();
             }
         });
 
@@ -103,11 +115,11 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onComplete() {
-
+                Log.d(TAG, "onComplete: DONE! :)");
             }
         });
 
-        InitialValueObservable<CharSequence> fourth = RxTextView.textChanges(mUsername);
+        InitialValueObservable<CharSequence> fourth = textChanges(mUsername);
 
         fourth.subscribe(new Observer<CharSequence>() {
             @Override
@@ -130,5 +142,47 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        RxTextView.textChangeEvents(email).subscribe(new Observer<TextViewTextChangeEvent>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull TextViewTextChangeEvent textViewTextChangeEvent) {
+                String text = textViewTextChangeEvent.text().toString();
+                boolean value = validate(text);
+                Log.d(TAG, "onNext: Is email valid? " + value);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+
+    // from https://stackoverflow.com/questions/8204680/java-regex-email
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    // from https://stackoverflow.com/questions/8204680/java-regex-email
+    public static boolean validate(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+        return matcher.find();
+    }
+
+    // from https://stackoverflow.com/questions/4238921/detect-whether-there-is-an-internet-connection-available-on-android
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
