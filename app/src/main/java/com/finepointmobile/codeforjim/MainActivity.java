11a5d582f339feb821fdf100f30af9d85ef9c7b7
Daniel Malone <danieljmalone@gmail.com>
2017-09-09 14:55:59 -0500

    more changes

--- app/src/main/java/com/finepointmobile/codeforjim/MainActivity.java
+++ app/src/main/java/com/finepointmobile/codeforjim/MainActivity.java
@@ -3,8 +3,26 @@ package com.finepointmobile.codeforjim;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.jakewharton.rxbinding2.InitialValueObservable;
+import com.jakewharton.rxbinding2.widget.RxTextView;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    EditText mUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +31,104 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mUsername = (EditText) findViewById(R.id.username);
 
+        Single first = Single.just("Hello");
+
+        first.subscribe(new SingleObserver() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d(TAG, "onSubscribe: ");
+            }
+
+            @Override
+            public void onSuccess(@NonNull Object o) {
+                Log.d(TAG, "onSuccess: " + o);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.d(TAG, "onError: ");
+            }
+        });
+
+        Observable second = Observable.fromArray(1, 2, 3, 4, 5);
+
+        second.subscribe(new Observer() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d(TAG, "onSubscribe: ");
+            }
+
+            @Override
+            public void onNext(@NonNull Object o) {
+                Log.d(TAG, "onNext: " + o);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.d(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        });
+
+        Observable third = Observable.create(new ObservableOnSubscribe() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter e) throws Exception {
+                for (int i = 500; i <= 1000; i++) {
+                    e.onNext(i);
+                }
+            }
+        });
+
+        third.subscribe(new Observer() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull Object o) {
+                Log.d(TAG, "onNext: " + o);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+        InitialValueObservable<CharSequence> fourth = RxTextView.textChanges(mUsername);
+
+        fourth.subscribe(new Observer<CharSequence>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull CharSequence charSequence) {
+                Log.d(TAG, "onNext: " + charSequence);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
     }
 }
